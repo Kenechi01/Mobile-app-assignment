7d59813c907102440521433467551806c6c215a4
Kenechi01 <chukwugrace023@gmail.com>
2026-04-27 00:03:37 +0100

    Initial commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,20 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { CustomTabBar } from '@/components/CustomTabBar';
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      tabBar={(props) => <CustomTabBar {...props} />}
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen name="home" options={{ title: 'Home' }} />
+      <Tabs.Screen name="wallet" options={{ title: 'Wallet' }} />
+      <Tabs.Screen name="referral" options={{ title: 'Referral' }} />
+      <Tabs.Screen name="plans" options={{ title: 'Plans' }} />
+      <Tabs.Screen name="more" options={{ title: 'More' }} />
+    </Tabs>
+  );
+}
---
+++ app/_layout.tsx
@@ -0,0 +1,16 @@
+import { DarkTheme, ThemeProvider } from '@react-navigation/native';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+export default function RootLayout() {
+  return (
+    <ThemeProvider value={DarkTheme}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+      <StatusBar style="light" />
+    </ThemeProvider>
+  );
+}
